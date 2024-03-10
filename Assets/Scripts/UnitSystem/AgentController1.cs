c61535eee753e2d2d15dd6b732995fc4aeb7cfab
yez69 <tymekmarek2@gmail.com>
2024-03-10 10:29:21 +0100

    Another reorganization.

--- Assets/Scripts/UnitSystem/AgentController1.cs
+++ Assets/Scripts/UnitSystem/AgentController1.cs
@@ -26,6 +26,7 @@ public class AgentController1 : MonoBehaviour
     private float backstabAngleThreshold = 135f;
     [SerializeField] GameObject weapon;
     private Animator weaponAnimator;
+    #region internal
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
@@ -34,7 +35,146 @@ public class AgentController1 : MonoBehaviour
         band = transform.parent.gameObject;
         weaponAnimator = weapon.GetComponent<Animator>();
     }
+    private void Update()
+    {
+        if (agent.remainingDistance <= agent.stoppingDistance && !agent.pathPending && gotUpdated)
+        {
+            OnDestinationReached();
+        }
+        if (Health <= 0 && isAlive)
+        {
+            warriorFallen();
+        }
+    }
+    private void OnDestinationReached()
+    {
+        Rotate(rotation);
+        gotUpdated = false;
+        FinishedPath = true;
+        weaponAnimator.SetTrigger("IdleTrig");
+    }
+    private void Rotate(Quaternion targetRotation)
+    {
+        if (rotationCoroutine != null)
+        {
+            StopCoroutine(rotationCoroutine);
+        }
+        rotationCoroutine = StartCoroutine(Rotator(targetRotation));
+    }
+    private void Rotate(Vector3 pos)
+    {
+        if (rotationCoroutine != null)
+        {
+            StopCoroutine(rotationCoroutine);
+        }
+        Quaternion targetRotation = Quaternion.LookRotation((pos - transform.position).normalized);
+        targetRotation.x = 0;
+        targetRotation.z = 0;
+        rotationCoroutine = StartCoroutine(Rotator(targetRotation));
+    }
+    IEnumerator Rotator(Quaternion targetRotation)
+    {
+        float t = 0;
+        while (t <= 1 && !RotateTaskStop)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, t);
+            t += rotationSpeed * Time.deltaTime;
 
+            yield return null;
+        }
+        RotateTaskStop = false;
+        Debug.Log("Stopping rotation");
+    }
+    private void warriorFallen()
+    {
+        isAlive = false;
+        Vector3 adjustedPos = transform.position;
+        adjustedPos.y = adjustedPos.y - .3f;
+        GameObject newCross = Instantiate(Tomb, adjustedPos, transform.rotation);
+        warriorRenderer.enabled = false;
+        warriorColider.enabled = false;
+        SetChildrenRenderers(false);
+        StopAllCoroutines();
+    }
+    private void SetChildrenRenderers(bool value)
+    {
+        foreach (Transform childTransform in transform)
+        {
+            if (childTransform.gameObject.GetComponent<MeshRenderer>() != null)
+            {
+                childTransform.gameObject.GetComponent<MeshRenderer>().enabled = value;
+            }
+            else
+            {
+                childTransform.GetChild(0).gameObject.GetComponent<MeshRenderer>().enabled = value;
+            }
+
+        }
+    }
+    #endregion internal
+    #region external
+    public void Attack(GameObject warrior)
+    {
+        StopAllCoroutines();
+        if (isAlive)
+        {
+            StartCoroutine(AttackTask(warrior, .1f));
+        }
+    }
+    IEnumerator AttackTask(GameObject warriorToAttack, float interval)
+    {
+        isAttacking = true;
+        while (!AttackTaskStop && isAlive)
+        {
+            yield return new WaitForSeconds(interval);
+            if (Vector3.Distance(transform.position, warriorToAttack.transform.position) > StoppingDistance)
+            {
+                agent.SetDestination(warriorToAttack.transform.position);
+            }
+            else
+            {
+                agent.ResetPath();
+                if (GetDotProduct(transform.gameObject, warriorToAttack) < 1f)
+                {
+                    Rotate(warriorToAttack.transform.position);
+                }
+                yield return new WaitForSeconds(1.6f);
+                if (GetDotProduct(warriorToAttack, transform.gameObject) < -.3f)
+                {
+                    warriorToAttack.GetComponent<AgentController1>().DealDamage(10, transform.gameObject);
+                    weaponAnimator.SetTrigger("CriticalTrig");
+                }
+                else
+                {
+                    warriorToAttack.GetComponent<AgentController1>().DealDamage(5, transform.gameObject);
+                    weaponAnimator.SetTrigger("SlashTrig");
+                }
+                if (!warriorToAttack.GetComponent<AgentController1>().isAlive)
+                {
+                    GameObject enemyWarband = warriorToAttack.transform.parent.gameObject;
+                    List<GameObject> warriorsByDistance = band.GetComponent<WarBandController1>().GetWarriorsInWarBandByDistance(enemyWarband, transform.position);
+                    bool found = false;
+                    foreach (GameObject enem in warriorsByDistance)
+                    {
+                        if (enem.GetComponent<AgentController1>().isAlive)
+                        {
+                            warriorToAttack = enem;
+                            found = true;
+                        }
+                    }
+                    if (!found)
+                    {
+                        AttackTaskStop = true;
+                    }
+                }
+            }
+            yield return new WaitForSeconds(interval);
+        }
+        isAttacking = false;
+        AttackTaskStop = false;
+        Debug.Log("Stopping attack");
+
+    }
     private float GetDotProduct(GameObject obj1, GameObject obj2)
     {
         // Kierunek obiektu 1
@@ -64,159 +204,14 @@ public class AgentController1 : MonoBehaviour
         GameObject enemyWarband = enemyWarrior.transform.parent.gameObject;
         band.GetComponent<WarBandController1>().GettingAttacked(enemyWarband);
     }
-    private void warriorFallen()
-    {
-        isAlive = false;
-        Vector3 adjustedPos = transform.position;
-        adjustedPos.y = adjustedPos.y - .3f;
-        GameObject newCross = Instantiate(Tomb, adjustedPos, transform.rotation);
-        warriorRenderer.enabled = false;
-        warriorColider.enabled = false;
-        SetChildrenRenderers(false);
-        StopAllCoroutines();
-    }
-
-    private void SetChildrenRenderers(bool value)
-    {
-        foreach(Transform childTransform in transform)
-        {
-            if (childTransform.gameObject.GetComponent<MeshRenderer>() != null)
-            {
-                childTransform.gameObject.GetComponent<MeshRenderer>().enabled = value;
-            }
-            else
-            {
-                childTransform.GetChild(0).gameObject.GetComponent<MeshRenderer>().enabled = value;
-            }
-            
-        }
-    }
-
-    private void Start()
-	{
-        EventManager.OnChunkMeshesInstanced.AddListener(() => { terrainGenerated = true; });
-	}
-	private void Update()
-    {
-        if (!terrainGenerated) return;
-        if (agent.remainingDistance <= agent.stoppingDistance && !agent.pathPending && gotUpdated)
-        {
-            OnDestinationReached();
-        }
-        if (Health <= 0 && isAlive)
-        {
-            warriorFallen();
-        }
-    }
-    IEnumerator Rotator(Quaternion targetRotation)
-    {
-        float t = 0;
-        while (t <= 1 && !RotateTaskStop)
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, t);
-            t += rotationSpeed * Time.deltaTime;
-
-            yield return null;
-        }
-        RotateTaskStop = false;
-        Debug.Log("Stopping rotation");
-    }
-
-    IEnumerator AttackTask( GameObject warriorToAttack, float interval)
-    {
-        isAttacking = true;
-        while (!AttackTaskStop && isAlive)
-        {
-            yield return new WaitForSeconds(interval);
-            if (Vector3.Distance(transform.position, warriorToAttack.transform.position) > StoppingDistance)
-            {
-                agent.SetDestination(warriorToAttack.transform.position);
-            }
-            else
-            {
-                agent.ResetPath();
-                if (GetDotProduct(transform.gameObject,warriorToAttack) < 1f)
-                {
-                    Rotate(warriorToAttack.transform.position);
-                }
-                yield return new WaitForSeconds(1.6f);
-                if(GetDotProduct(warriorToAttack, transform.gameObject) < -.3f)
-                {
-                    warriorToAttack.GetComponent<AgentController1>().DealDamage(10, transform.gameObject);
-                    weaponAnimator.SetTrigger("CriticalTrig");
-                }
-                else
-                {
-                    warriorToAttack.GetComponent<AgentController1>().DealDamage(5, transform.gameObject);
-                    weaponAnimator.SetTrigger("SlashTrig");
-                }
-                if (!warriorToAttack.GetComponent<AgentController1>().isAlive)
-                {
-                    GameObject enemyWarband = warriorToAttack.transform.parent.gameObject;
-                    List<GameObject> warriorsByDistance = band.GetComponent<WarBandController1>().GetWarriorsInWarBandByDistance(enemyWarband, transform.position);
-                    bool found = false;
-                    foreach(GameObject enem in warriorsByDistance)
-                    {
-                        if (enem.GetComponent<AgentController1>().isAlive)
-                        {
-                            warriorToAttack = enem;
-                            found = true;
-                        }
-                    }
-                    if (!found)
-                    {
-                        AttackTaskStop = true;
-                    }
-                }
-            }
-            yield return new WaitForSeconds(interval);
-        }
-        isAttacking = false;
-        AttackTaskStop = false;
-        Debug.Log("Stopping attack");
-        
-    }
-    public void Attack(GameObject warrior)
-    {
-        StopAllCoroutines();
-        if (isAlive)
-        {
-            StartCoroutine(AttackTask(warrior, .1f));
-        }
-    }
-
-    private void Rotate(Quaternion targetRotation)
-    {
-        if(rotationCoroutine != null){
-            StopCoroutine(rotationCoroutine);
-        }
-        rotationCoroutine = StartCoroutine(Rotator(targetRotation));
-    }
-    private void Rotate(Vector3 pos)
-    {
-        if (rotationCoroutine != null)
-        {
-            StopCoroutine(rotationCoroutine);
-        }
-        Quaternion targetRotation = Quaternion.LookRotation((pos - transform.position).normalized);
-        targetRotation.x = 0;
-        targetRotation.z = 0;
-        rotationCoroutine = StartCoroutine(Rotator(targetRotation));
-    }
     public void MoveToDestination(Vector3 pos, Quaternion targetRotation)
     {
-		gotUpdated = true;
+        gotUpdated = true;
         FinishedPath = false;
         rotation = targetRotation;
         agent.SetDestination(pos);
         StopAllCoroutines();
         isAttacking = false;
     }
-    private void OnDestinationReached()
-    {
-        Rotate(rotation);
-        gotUpdated = false;
-        FinishedPath = true;
-        weaponAnimator.SetTrigger("IdleTrig");
-    }
+    #endregion external
 }
